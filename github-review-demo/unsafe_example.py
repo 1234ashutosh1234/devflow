643ecb46075b1ea5b8efd93206d4505088b1492d
Ashutosh Raj <ashu953442@gmail.com>
2026-09-07 00:52:03 +0530

    Add AI review test fixture

---
+++ github-review-demo/unsafe_example.py
@@ -0,0 +1,10 @@
+def process_users(users):
+    api_key = "SECRET-12345"
+
+    for i in range(len(users)):
+        print(users[i])
+
+    eval("print('dangerous')")
+
+    # TODO: improve this
+    return users
